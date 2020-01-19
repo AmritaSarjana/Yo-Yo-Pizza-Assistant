9b0d47ce66cda0598eca131549677e91bfaff08d
Amrita <amrita.mohanta@tracxn.com>
2020-01-19 21:19:10 +0530

    testing

--- db/mongoConnection.js
+++ db/mongoConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('c+srv://Amrita:wH9gbcIbNddFVWXx@cluster0-oeilr.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://Amrita:wH9gbcIbNddFVWXx@cluster0-oeilr.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 module.exports = mongoose;
